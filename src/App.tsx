27943f1aa7edcc33e8f7f3005db474e1a7f02a00
Olakunle Raheem <olaks.raheem@gmail.com>
2023-10-20 10:07:28 +0100

    New changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export default function App () {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/tickets" element={<Tickets />} />
+        <Route path="tickets" element={<Tickets />} />
       </Routes>
     </Router>
   )
